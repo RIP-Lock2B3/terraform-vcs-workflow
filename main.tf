803ac20d3cf41dd61e758a77f0a3c648bde5fa8d
atom_1106 <nani.bhogapurapu1106@gmail.com>
2024-11-30 10:17:42 +0000

    module fix

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
 
 module "apache-example" {
   source         = "RIP-Lock2B3/apache-example/aws"
-  version        = "1.0.0"
+  version        = "~> 1.0.0"
   my_ip_cidr     = var.my_ip_cidr
   default_vpc_id = var.default_vpc_id
   public_key     = var.public_key
