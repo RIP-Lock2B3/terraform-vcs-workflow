8c198dfd6bf78cfa05db7ebc910a84fe5ba30668
atom_1106 <nani.bhogapurapu1106@gmail.com>
2024-11-30 09:27:31 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+
+}
+
+module "apache-example" {
+  source         = "RIP-Lock2B3/apache-example/aws"
+  version        = "1.0.0"
+  my_ip_cidr     = var.my_ip_cidr
+  default_vpc_id = var.default_vpc_id
+  public_key     = var.public_key
+  instance_type  = var.instance_type
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "default_vpc_id" {
+  type = string
+}
+
+variable "my_ip_cidr" {
+  type = string
+}
+
+variable "public_key" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
